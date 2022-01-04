9ff03751d9856b48a4143f999bca3900b49212dd
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-01-04 21:46:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+// Resolve persisting error message "Uncaught ReferenceError: __dirname is not defined"
+const path = require('path');
+let __dirname = path.resolve();
+
+const http = require('http'),
+  url = require('url'),
+  fs = require('fs');
+
+http.createServer((request, response) => {
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+
+console.log('My first Node test server is running on Port 8080.');
