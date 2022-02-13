469a44427ad4a7b70a44fd0864b36de6eb9eff9e
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-13 21:21:11 +0100

    Update allowedOrigins

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 const cors = require('cors');
 
 // By default, CORS will set the application to allow requests from all origins; however, if only certain origins shall be given access, "app.use(cors());" needs to be replaced with the following:
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com', 'https://i.discogs.com/'];
 
 app.use(cors({
   origin: (origin, callback) => {
